6d1065803c561dac22ad9b99f737b9d3d06d0be7
Digital <Digital@DESKTOP-7LE7F73>
2023-04-13 22:32:57 +0200

    Second Commit-Add project files.

---
+++ pracWPF/View/CutomersView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using pracWPF.Data;
+using pracWPF.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace pracWPF.View
+{
+    public partial class CutomersView : UserControl
+    {
+        private CustomersViewModel _viewModel;
+
+        public CutomersView()
+        {
+            InitializeComponent();
+            _viewModel=new CustomersViewModel(new CustomerDataProvider());
+            DataContext = _viewModel;
+            Loaded += CutomersView_Loaded;
+        }
+
+        
+
+        private async void CutomersView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            var column = (int)CustomerListGrid.GetValue(Grid.ColumnProperty);
+            var newColumn = column == 0 ? 2 : 0;
+            CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Add();
+        }
+    }
+}
